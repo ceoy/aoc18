e5155b35366092e0477bff36a77407c0c1f9e15d
Tim Jahn <ceoymw@gmail.com>
2019-01-01 15:45:50 +0200

    wip: day17

--- src/main/kotlin/com/ceoy/aoc/Day17.kt
+++ src/main/kotlin/com/ceoy/aoc/Day17.kt
@@ -3,6 +3,83 @@ package com.ceoy.aoc
 class Day17(private val input: List<String>) {
 
     fun partOne(): Int {
+
+        val grid = createAreaToFill(input)
+
+        visualizeGrid(grid)
+
         return 0
+    }
+
+    private fun visualizeGrid(grid: List<List<Type>>) {
+        grid.forEach {
+            it.forEach { type ->
+                print(type.renderChar)
+            }
+            println("")
+        }
+    }
+
+    private fun createAreaToFill(input: List<String>): List<List<Type>> {
+        // parse lines first
+        val allClay = input.flatMap {
+            Clay.parse(it)
+        }
+
+        val maxY = allClay.maxBy { it.y }!!.y
+        val maxX = allClay.maxBy { it.x }!!.x + 1 // add one, because water could flow down next to it
+
+        val grid = mutableListOf<MutableList<Type>>()
+        (0..maxY).forEach { y ->
+            grid.add(mutableListOf())
+            (0..maxX).forEach { x ->
+                if (allClay.find { it.x == x && it.y == y } != null) {
+                    grid[y].add(Type.CLAY)
+                } else {
+                    grid[y].add(Type.SAND)
+                }
+            }
+        }
+
+        grid[0][500] = Type.RESERVOIR
+
+        return grid
+    }
+
+    data class Clay(val x: Int, val y: Int) {
+        companion object {
+            fun parse(line: String): List<Clay> {
+                val allTheClay = mutableListOf<Clay>()
+
+                val values = line
+                    .replace("x=", "")
+                    .replace(",", "")
+                    .replace("y=", "")
+                    .replace("..", " ")
+                    .split(" ")
+                    .map(String::toInt)
+                    .toIntArray()
+
+                if (line[0] == 'x') {
+                    (values[1]..values[2]).map {
+                        allTheClay.add(Clay(values[0], it))
+                    }
+                } else {
+                    (values[1]..values[2]).map {
+                        allTheClay.add(Clay(it, values[0]))
+                    }
+                }
+
+                return allTheClay
+            }
+        }
+    }
+
+    enum class Type(val renderChar: Char) {
+        SAND('.'),
+        CLAY('#'),
+        RESTING_WATER('~'),
+        FALLING_WATER('|'),
+        RESERVOIR('+')
     }
 }
--- src/test/kotlin/com/ceoy/aoc/Day17Test.kt
+++ src/test/kotlin/com/ceoy/aoc/Day17Test.kt
@@ -5,10 +5,17 @@ import org.junit.Test
 class Day17Test {
 
     private val actualInput = FileLoader.load("day17.txt")
+    private val exampleInput = FileLoader.load("day17_example.txt")
 
     @Test
     fun `Day 17 Part One Actual`() {
         val day17 = Day17(actualInput)
         println(day17.partOne())
+    }
+
+    @Test
+    fun `Day 17 Part One Example`() {
+        val day17 = Day17(exampleInput)
+        day17.partOne()
     }
 }
